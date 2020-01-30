adc84e98450d9c2de98a9601be3e83c8545f5579
sunlongyu <1223956215@qq.com>
2020-01-30 17:02:13 +0800

    navigation init

---
+++ app/src/main/java/com/hua/navigationdemo/MainActivity.java
@@ -0,0 +1,28 @@
+package com.hua.navigationdemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        NavController controller = Navigation.findNavController(this, R.id.fragment);
+        NavigationUI.setupActionBarWithNavController(this, controller);
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        NavController controller = Navigation.findNavController(this, R.id.fragment);
+        boolean b = controller.navigateUp();
+        return b;
+//        return super.onSupportNavigateUp();
+
+    }
+}
